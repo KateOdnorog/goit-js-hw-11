5e39bd56d704cc5d893702ee08ca72130e6c2eb7
Kateryna Odnoroh <katya.odnorog@gmail.com>
2025-02-17 23:48:54 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,2 +1,48 @@
-import {} from './js/pixabay-api.js';
-import {} from './js/render-functions.js';
+import findImages from './js/pixabay-api.js';
+import createHTML from './js/render-functions.js';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  form: document.querySelector('.form'),
+  btnSearch: document.querySelector('.btn-search'),
+  input: document.querySelector('.input-field'),
+  loader: document.querySelector('.loader'),
+  gallery: document.querySelector('.gallery'),
+};
+
+refs.loader.classList.add('hidden');
+
+refs.form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const searchText = refs.input.value;
+
+  if (searchText === '') {
+    return iziToast.error({
+      position: 'topRight',
+      title: 'ERROR',
+      message: 'You have not specified any search parameters.',
+    });
+  }
+
+  refs.loader.classList.remove('hidden');
+
+  findImages(searchText)
+    .then(data => {
+      refs.gallery.innerHTML = '';
+      createHTML(data.data.hits);
+    })
+    .catch(err => {
+      iziToast.error({
+        title: 'ERROR',
+        message: `Sorry, there are no images matching your search query. Please try again!`,
+        position: 'topRight',
+      });
+    });
+
+  e.target.reset();
+}
